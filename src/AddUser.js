ab76a72e0b3f9d5711e1d84030734755e78e5e03
Neelam Chavan <neelamchavan410@gmail.com>
2022-04-04 22:13:15 +0530

    add form validation

--- src/AddUser.js
+++ src/AddUser.js
@@ -15,13 +15,17 @@ function AddUser(props) {
         props.addUserHandler(username, age)
         setUsername("")
         setAge("")
+
+        if (age < 0 || age > 110) {
+            alert("Please enter a valid age")
+        }
     }
 
     return (
         <div style={{ width: '60%', background: '#35413d', color: 'white', padding: '2em', display: 'flex', justifyContent: 'center' }}>
             <form onSubmit={onSubmitHandler}>
                 <label style={{ display: 'block', marginBottom: '8px' }} > Username</label>
-                <input type="text" value={username} onChange={usernameChangeHandler} /><br /><br />
+                <input type="text" maxLength={20} value={username} onChange={usernameChangeHandler} /><br /><br />
                 <label style={{ display: 'block', marginBottom: '8px' }}>Age</label>
                 <input type="number" value={age} onChange={ageChangeHandler} style={{ display: 'block' }} />
                 <button style={{ marginTop: '1em' }}>Submit</button>
